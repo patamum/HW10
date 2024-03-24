f60b76f0406d67e7d2c25313839b0d30c0fe1131
Svetlana Jourkova <belkina_s@mail.ru>
2024-03-25 00:15:29 +0300

    Добавьте файлы проекта.

---
+++ Task10_1/Angle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_1
+{
+    class Angle
+    {
+        int grade;
+        int minute;
+        int second;
+        public int Grade { get; set; }
+        
+        public int Minute
+        { get 
+            { 
+                return minute; 
+            } 
+          set 
+            {
+                Grade += value / 60;
+                minute = value % 60;
+            } 
+        }
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                Minute += value / 60;
+                second = value % 60;
+            }
+        }
+        public Angle(int grade, int minute, int second)
+        {
+            this.grade = grade;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        public double ToRadian()
+        {
+            return ((grade + (double) minute / 60 + (double)second / 3600) * (Math.PI)) / 180;
+        }
+
+    }
+}
---
+++ Task10_1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите градусы");
+            int grade_ = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите минуты");
+            int minute_ = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите секунды");
+            int second_ = Convert.ToInt32(Console.ReadLine());
+            Angle angle = new Angle(grade_, minute_, second_);
+            double totalRadian = angle.ToRadian();
+            Console.WriteLine($"{grade_} ° {minute_}' {second_}'' = {totalRadian} рад");
+        }
+    }
+}
